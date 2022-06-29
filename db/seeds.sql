32d8934205249fb8ff8c12aaf888465c6b416ecc
Allen <almcbroombey@hotmail.com>
2022-06-28 22:58:58 -0400

    added employee table to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,13 @@
 USE company_db
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES
+  (1, 'Vivian', 'Walker', 1),
+  (2, 'Sabir', 'Moorer', 0),
+  (3, 'Caesar', 'Lopez', 1),
+  (4' 'Katrina', 'Smith', 1),
+  (5, 'Dora', 'Summers', 0),
+  (6, 'Edward', 'Bellamy', 0),
+  (7, 'Melissa', 'Holloway', 1),
+  (8, 'Octavia', 'Brown', 1),
+  (9, 'Thurgood', 'Ali', 1);
