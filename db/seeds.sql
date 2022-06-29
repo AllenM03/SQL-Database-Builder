371e7149247bc8448229c94c83caf8ecb3cd6055
Allen <almcbroombey@hotmail.com>
2022-06-29 18:29:33 -0400

    added connection.js file and dotenv syntax, adds function validators for user input

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,9 +5,11 @@ VALUES
   (1, 'Vivian', 'Walker', 1),
   (2, 'Sabir', 'Moorer', 0),
   (3, 'Caesar', 'Lopez', 1),
-  (4' 'Katrina', 'Smith', 1),
+  (4, 'Katrina', 'Smith', 1),
   (5, 'Dora', 'Summers', 0),
   (6, 'Edward', 'Bellamy', 0),
   (7, 'Melissa', 'Holloway', 1),
   (8, 'Octavia', 'Brown', 1),
   (9, 'Thurgood', 'Ali', 1);
+
+INSERT INTO department ()
